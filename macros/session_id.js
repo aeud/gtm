dc072ba1d83f32c8a1c7b58a61188896b87a8952
aeud <adrien.eudes@gmail.com>
2014-12-19 16:20:13 +0800

    Change session script

--- macros/session_id.js
+++ macros/session_id.js
@@ -1,14 +1,12 @@
 function () {
-	return {{cookie}} || (function(){
-		return (function (name, value, expires, path, domain, secure) {
-			var today = new Date();
-			var expires_date = new Date( today.getFullYear(), today.getMonth(), parseInt(today.getDate()) + parseInt(expires) );
-			document.cookie= name + "=" + escape(value) +
-			((expires) ? "; expires=" + expires_date.toGMTString() : "") +
-			((path) ? "; path=" + path : "") +
-			((domain) ? "; domain=" + domain : "") +
-			((secure) ? "; secure" : "");
-			return value;
-		})('__id', {{random}}, 700);
-	})();
+	return {{cookie value}} || (function (name, value, expires, path, domain, secure) {
+		var today = new Date();
+		var expires_date = new Date( today.getFullYear(), today.getMonth(), parseInt(today.getDate()) + parseInt(expires) );
+		document.cookie= name + "=" + escape(value) +
+		((expires) ? "; expires=" + expires_date.toGMTString() : "") +
+		((path) ? "; path=" + path : "") +
+		((domain) ? "; domain=" + domain : "") +
+		((secure) ? "; secure" : "");
+		return value;
+	}({{cookie name}}, {{random}}, 700, null, {{url hostname}}, null);
 }
